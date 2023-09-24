8ad3d17faade26d9610390b41e91e289157b85c3
Tushar Saxena <tusharsaxena771@gmail.com>
2023-09-24 18:14:56 +0530

    introduced validations and created createUserAccount functionality

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import HomeLayout from "../Layouts/HomeLayout";
 import { BsPersonCircle } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import {toast} from "react-hot-toast";
 
 function Signup() {
 const dispatch = useDispatch();
@@ -28,6 +29,7 @@ const navigate=useNavigate();
     })
   }
 
+
 //      Handler for Profile Image
 
   function handleProfileImage(e){
@@ -47,6 +49,29 @@ const navigate=useNavigate();
     }
   }
 
+    // Function to create new UserAccount(Signup)
+
+    function createNewUser(event){
+        let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        event.preventDefault();
+        if(!signupData.email || !signupData.password || !signupData.fullName || !signupData.avatar){
+            toast.error("Please fill all the details.")
+            return;
+        }
+        // Check length of name field
+        if(signupData.fullName.length<5){
+            toast.error("Full name should be of atleast of length five.")
+            return;
+        }
+        if(!signupData.email.match(mailformat)){
+            toast.error("Please enter a valid email");
+            return;
+        }
+
+
+
+    }
+
 
 
 
@@ -56,6 +81,8 @@ const navigate=useNavigate();
       <div className="flex items-center justify-center h-[90vh]">
         {/* FORM */}
         <form
+          noValidate
+          onSubmit={createNewUser}
           className="flex flex-col justify-center  gap-3 rounded-lg p-4 text-white w-96 
         shadow-[0_0_10px_black]"
         >
